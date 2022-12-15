07de3bbb7b31db9066726b1b251ed0443a0c2426
SkullZRulZ <nero310@hotmail.fr>
2022-12-15 10:23:20 +0100

    add DELETE feature

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const port = process.env.APP_PORT ?? 5000;
+const port = process.env.APP_PORT ?? 5500;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -14,6 +14,8 @@ app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
 
+// const { validateUser } = require("./validators.js");
+
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", movieHandlers.getUsers);
@@ -22,7 +24,8 @@ app.post("/api/movies", movieHandlers.postMovie);
 app.post("/api/users", movieHandlers.postUser);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.put("/api/users/:id", movieHandlers.updateUser);
-
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+app.delete("/api/users/:id", movieHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
--- movieHandlers.js
+++ movieHandlers.js
@@ -1,33 +1,6 @@
-const movies = [
-  {
-    id: 1,
-    title: "Citizen Kane",
-    director: "Orson Wells",
-    year: "1941",
-    colors: false,
-    duration: 120,
-  },
-  {
-    id: 2,
-    title: "The Godfather",
-    director: "Francis Ford Coppola",
-    year: "1972",
-    colors: true,
-    duration: 180,
-  },
-  {
-    id: 3,
-    title: "Pulp Fiction",
-    director: "Quentin Tarantino",
-    year: "1994",
-    color: true,
-    duration: 180,
-  },
-];
-
 const database = require("./database");
 
-const getMovies = (req, res) => {
+const getMovies = (_req, res) => {
   database
     .query("select * from movies")
     .then(([movies]) => {
@@ -57,7 +30,7 @@ const getMovieById = (req, res) => {
     });
 };
 
-const getUsers = (req, res) => {
+const getUsers = (_req, res) => {
   database
     .query("select * from users")
     .then(([users]) => {
@@ -165,6 +138,42 @@ const updateUser = (req, res) => {
     });
 };
 
+const deleteMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("delete from movies where id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error deleting the movie");
+    });
+};
+
+const deleteUser = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("delete from users where id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error deleting the user");
+    });
+};
+
 module.exports = {
   getMovies,
   getMovieById,
@@ -174,4 +183,6 @@ module.exports = {
   postUser,
   updateMovie,
   updateUser,
+  deleteMovie,
+  deleteUser,
 };
